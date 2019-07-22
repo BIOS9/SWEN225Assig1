1cb2587a1720f6f9c3ed8704f1e493d387d7448c
NightFish <NightFish.net@gmail.com>
2019-07-22 20:29:18 +1200

    Adds method outline for cell

--- source/cluedo/src/Cell.java
+++ source/cluedo/src/Cell.java
@@ -9,5 +9,40 @@
  *
  */
 class Cell {
+    private final Room room;
+    private final Cell north, south, east, west;
+    private Character occupant;
 
+    /**
+     * Constructor
+     * @param room The room type of this cell
+     * @param north North neighbouring cell, or null
+     * @param south South neighbouring cell, or null
+     * @param east East neighbouring cell, or null
+     * @param west West neighbouring cell, or null
+     */
+    public Cell(Room room, Cell north, Cell south, Cell east, Cell west) {
+        this.room = room;
+        this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;
+    }
+
+    /**
+     * Checks if specified room matches the current room of the cell
+     * @param room
+     * @return
+     */
+    public boolean isRoom(Room room) {
+        return this.room == room;
+    }
+
+    /**
+     * Checks if the cell is occupied by a player
+     * @return
+     */
+    public boolean isOccupied() {
+        return occupant != null;
+    }
 }
