9dcd2409ef91c240a8706046509f5335da59ce39
NightFish <NightFish.net@gmail.com>
2019-07-23 00:34:36 +1200

    Refactors package and implements rollDice method

--- source/cluedo/src/game/Turn.java
+++ source/cluedo/src/game/Turn.java
@@ -1,11 +1,24 @@
+package game;
+import java.util.*;
+
 class Turn {
-    public static final DICE_COUNT = 2;
-    public static final DICE_SIDES = 6;
+    public static final int DIE_COUNT = 2;
+    public static final int DIE_SIDES = 6;
 
     private final int diceRoll;
     private Player player;
     private Queue<Cell> moves;
     private Suggestion suggestion;
+
+    /**
+     * Constructor
+     * Rolls dice, configures variables
+     * @param player Player who is the owner of this turn
+     */
+    public Turn(Player player) {
+        diceRoll = rollDice();
+        this.player = player;
+    }
 
     /**
      * Checks if the turn is valid
@@ -22,6 +35,12 @@ class Turn {
      * @return integer value representing dice roll
      */
     public int rollDice() {
-        // Use dice constants to generate random dice roll
+        Random rng = new Random();
+        int roll = 0;
+
+        for(int die = 0; die < DIE_COUNT; ++die)
+            roll += rng.nextInt(DIE_SIDES) + 1; // Add 1 because rng gives 0-exclusive DIE_SIDES, dice don't have 0
+
+        return roll;
     }
 }
