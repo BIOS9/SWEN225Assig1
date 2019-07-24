87e20008e3ba8fc139b5cd26ac5aa0d1c9b75056
NightFish <NightFish.net@gmail.com>
2019-07-24 20:28:38 +1200

    Adds board layout string

--- source/cluedo/src/game/board/Board.java
+++ source/cluedo/src/game/board/Board.java
@@ -1,4 +1,43 @@
 package game.board;
 
 public class Board {
+    // k = Kitchen
+    // b = Ball room
+    // c = Conservatory
+    // p = Billiard/Pool room
+    // h = Hallway
+    // 0-6 = Character starting positions
+    // d = Dining room
+    // l = Library
+    // r = Hall
+    // t = Lounge
+    // s = Study
+
+    // Capital letters represent doorways CAREFUL OF DOUBLE WIDTH DOORWAYS
+    private static final String BOARD =
+            "         0    1         \n" +
+            "kkkkkk hhhbbbbhhh cccccc\n" +
+            "kkkkkkhhbbbbbbbbhhcccccc\n" +
+            "kkkkkkhhbbbbbbbbhhcccccc\n" +
+            "kkkkkkhhbbbbbbbbhhCccccc\n" +
+            "kkkkkkhHBbbbbbbBHhHcccc \n" +
+            " kkkKkhhbbbbbbbbhhhhhhh2\n" +
+            "hhhhHhhhbBbbbbBbhhhhhhh \n" +
+            " hhhhhhhhHhhhhHhhhpppppp\n" +
+            "dddddhhhhhhhhhhhhHPppppp\n" +
+            "ddddddddhh     hhhpppppp\n" +
+            "ddddddddhh     hhhpppppp\n" +
+            "dddddddDHh     hhhppppPp\n" +
+            "ddddddddhh     hhhhhHhH \n" +
+            "ddddddddhh     hhhllLll \n" +
+            "ddddddddhh     hhlllllll\n" +
+            " hhhhhhhhh     hHLllllll\n" +
+            "5hhhhhhhhhhHHhhhhlllllll\n" +
+            " hhhhhHhhrrRRrrhhhlllll \n" +
+            "ttttttThhrrrrrrhhhhhhhh3\n" +
+            "ttttttthhrrrrrRHhHhhhhh \n" +
+            "ttttttthhrrrrrrhhSssssss\n" +
+            "ttttttthhrrrrrrhhsssssss\n" +
+            "ttttttthhrrrrrrhhsssssss\n" +
+            "tttttt 4 rrrrrr h ssssss\n";
 }
