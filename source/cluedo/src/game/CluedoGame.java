3d977d616ddb2ebe795f80e6492403846f9aee45
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-07-28 21:31:17 +1200

    Finds soloution cards

--- source/cluedo/src/game/CluedoGame.java
+++ source/cluedo/src/game/CluedoGame.java
@@ -1,12 +1,16 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 import game.board.Board;
 import game.cards.Card;
+import game.cards.Character;
 import game.cards.Room;
+import game.cards.Weapon;
 
 /**
  * CluedoGame is responsible for initiating the game board, cards and players
@@ -23,13 +27,19 @@ class CluedoGame {
 
 	// CluedoGame Associations
 	private Board board;
+	private game.cards.Character murderer;
+	private game.cards.Weapon murderWeapon;
+	private game.cards.Room murderRoom;
 
-	private final game.cards.Character[] characters = { new game.cards.Character("Miss Scarlett"),
-			new game.cards.Character("Rev. Green"), new game.cards.Character("Colonel Mustard"),
-			new game.cards.Character("Professor Plum"), new game.cards.Character("Mrs. Peacock"),
+	private final game.cards.Character[] characters = { 
+			new game.cards.Character("Miss Scarlett"),
+			new game.cards.Character("Rev. Green"),
+			new game.cards.Character("Colonel Mustard"),
+			new game.cards.Character("Professor Plum"),
+			new game.cards.Character("Mrs. Peacock"),
 			new game.cards.Character("Mrs. White") };
 
-	private final game.cards.Room[] roomCards = {
+	private final game.cards.Room[] rooms = {
     		new game.cards.Room("Kitchen", 'k'),
     		new game.cards.Room("Ball Room", 'b'),
     		new game.cards.Room("Conservatory", 'c'),
@@ -71,15 +81,30 @@ class CluedoGame {
 	 * Generates the cards, selects a solution (3 cards) shuffles remaing cards and
 	 * deals them to the players.
 	 */
-	public void initCards() {
+	private void initCards() {
 		// Create three stack of cards, one for each card type, Sets since order doesnt
 		// matter and no dups?
-
-		// pass to Card to populate the sets? then merge into one set.
-
-		// Get random card of each type to make solution
-		// Shuffle remaining cars into one stack
-		// Deal cards to players
+		List<Character> characterCards = new ArrayList<Character>();
+		List<Room> roomCards = new ArrayList<Room>();
+		List<Weapon> weaponCards = new ArrayList<Weapon>();
+		
+		//Choosing the character soloution
+		Collections.shuffle(characterCards);
+		this.murderer = characterCards.get(0);
+		characterCards.remove(0);
+		
+		//Choosing the room soloution
+		Collections.shuffle(roomCards);
+		this.murderRoom = roomCards.get(0);
+		roomCards.remove(0);
+		
+		//Choosing the weapon soloution;
+		Collections.shuffle(weaponCards);
+		this.murderWeapon = weaponCards.get(0);
+		weaponCards.remove(0);
+		
+		List<Card> allCards = new ArrayList<>();
+		
 
 	}
 }
