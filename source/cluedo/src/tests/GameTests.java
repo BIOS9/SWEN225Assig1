390c91fa4d6c7edb0dffe30f921b19c5d7aa5241
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-08-01 21:32:48 +1200

    Adds print hand to player and prints out at turn start

---
+++ source/cluedo/src/tests/GameTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import game.CluedoGame;
+import game.board.Board;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class GameTests {
+	
+	CluedoGame game = new CluedoGame();
+	
+	
+
+}
