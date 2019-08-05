41859ef5ee1616e58d9c1148e19668bb41d06bbd
NightFish <NightFish.net@gmail.com>
2019-08-05 20:48:41 +1200

    Adds card tests

--- source/cluedo/src/tests/CardTests.java
+++ source/cluedo/src/tests/CardTests.java
@@ -3,7 +3,9 @@ package tests;
 import game.board.Board;
 import game.board.Cell;
 import game.board.Position;
+import game.cards.Character;
 import game.cards.Room;
+import game.cards.Weapon;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,4 +40,48 @@ public class CardTests {
             new game.cards.Weapon("Revolver"),
             new game.cards.Weapon("Rope"),
             new game.cards.Weapon("Spanner") };
+
+    @Test
+    void testCardGetName() {
+        Weapon w = weapons[0];
+        game.cards.Character c = characters[0];
+        Room r = rooms[0];
+
+        assertEquals(w.getName(), "Candlestick");
+        assertEquals(c.getName(), "Miss Scarlett");
+        assertEquals(r.getName(), "Kitchen");
+    }
+
+    @Test
+    void testCharacterEquals() {
+        assertTrue(characters[0].equals(new Character("Miss Scarlett", 1)));
+
+        assertTrue(characters[0].equals(characters[0]));
+        assertFalse(characters[0].equals(characters[1]));
+        assertFalse(characters[0].equals(null));
+
+        // Character number should have no effect on equality
+        assertTrue(characters[0].equals(new Character("Miss Scarlett", 99)));
+    }
+
+    @Test
+    void testRoomEquals() {
+        assertTrue(rooms[0].equals(new Room("Kitchen", 'K')));
+
+        assertTrue(rooms[0].equals(rooms[0]));
+        assertFalse(rooms[0].equals(rooms[1]));
+        assertFalse(rooms[0].equals(null));
+
+        // Room prefix should have no effect on equality
+        assertTrue(rooms[0].equals(new Room("Kitchen", 'A')));
+    }
+
+    @Test
+    void testWeaponEquals() {
+        assertTrue(weapons[0].equals(new Weapon("Candlestick")));
+
+        assertTrue(weapons[0].equals(weapons[0]));
+        assertFalse(weapons[0].equals(weapons[1]));
+        assertFalse(weapons[0].equals(null));
+    }
 }
