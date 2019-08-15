76796c9deacb31ffb8af4103b2da412a42783b48
NightFish <NightFish.net@gmail.com>
2019-08-16 00:12:47 +1200

    Adds basic textures to GUI

--- source/cluedo/src/gui/GameWindow.java
+++ source/cluedo/src/gui/GameWindow.java
@@ -1,39 +1,86 @@
 package gui;
 
 import game.CluedoGame;
+import gui.Update.DiceUpdate;
 import gui.request.PlayerCountRequest;
 import gui.request.PlayerRequest;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
-public class GameWindow extends JFrame implements Observer, ActionListener {
-    public static final String WINDOW_TITLE = "Cluedo";
-    public static final int WINDOW_INITIAL_WIDTH = 800;
-    public static final int WINDOW_INITIAL_HEIGHT = 800;
-    public static final String DICE_BACKGROUND_IMAGE = "images/felt.jpg";
+        public class GameWindow extends JFrame implements Observer, ActionListener {
+            public static final String WINDOW_TITLE = "Cluedo";
+            public static final int
+                    WINDOW_INITIAL_WIDTH = 800,
+                    WINDOW_INITIAL_HEIGHT = 800,
+
+                    DICE_BOX_WIDTH = 200,
+                    DICE_BOX_HEIGHT = 100,
+                    DICE_SIZE = 60,
+                    DICE_GAP = 20,
+                    DICE_PADDING_TOP = 15;
+
+            public static final Map<String, String> IMAGE_FILES = new HashMap<String, String>() {{
+                put("felt", "images/texture/felt.jpg");
+                put("darkFelt", "images/texture/darkFelt.jpg");
+                put("borderTL", "images/texture/TopLeftBorderCorner.png");
+                put("borderTR", "images/texture/TopRightBorderCorner.png");
+                put("borderBL", "images/texture/BottomLeftBorderCorner.png");
+                put("borderBR", "images/texture/BottomRightBorderCorner.png");
+                put("borderTop", "images/texture/TopBorderTile.png");
+                put("borderBottom", "images/texture/BottomBorderTile.png");
+                put("borderLeft", "images/texture/LeftBorderTile.png");
+                put("borderRight", "images/texture/RightBorderTile.png");
+                put("die1", "images/dice/die1.png");
+                put("die2", "images/dice/die2.png");
+                put("die3", "images/dice/die3.png");
+                put("die4", "images/dice/die4.png");
+                put("die5", "images/dice/die5.png");
+                put("die6", "images/dice/die6.png");
+    }};
+    private Map<String, Image> images = new HashMap<>();
 
     private CluedoGame game = null;
 
-    private JLabel diceBox, cardBox, playerBox, boardBox, messageBox, infoBox;
-
+    private JLabel boardBox, messageBox, infoBox;
+    private ImagePanel cardBox, diceBox, die1, die2, playerBox;
 
     public GameWindow() {
         super(WINDOW_TITLE);
         setSize(WINDOW_INITIAL_WIDTH, WINDOW_INITIAL_HEIGHT);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        loadImages();
         buildWindow();
 
         // Finalize and display the window with the game
         pack();
         setVisible(true);
         //newGame();
+    }
+
+    /**
+     * Loads images from files into image map
+     */
+    private void loadImages() {
+        for(Map.Entry<String, String> image : IMAGE_FILES.entrySet()) {
+            try {
+                images.put(image.getKey(), ImageIO.read(new File(image.getValue())));
+            } catch (IOException ex) {
+                System.out.println("ERROR LOADING IMAGE: " + ex + " " + image.getValue());
+            }
+        }
     }
 
     /**
@@ -86,10 +133,8 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
     }
 
     private void buildPlayerBox(JPanel container) {
-        playerBox = new JLabel();
+        playerBox = new ImagePanel(images.get("darkFelt"), images.get("borderTL"), images.get("borderTR"), images.get("borderBL"), images.get("borderBR"), images.get("borderTop"), images.get("borderBottom"), images.get("borderLeft"), images.get("borderRight"));
         playerBox.setPreferredSize(new Dimension(200, 100));
-        playerBox.setOpaque(true);
-        playerBox.setBackground(Color.orange);
 
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.VERTICAL;
@@ -101,10 +146,21 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
     }
 
     private void buildDiceBox(JPanel container) {
-        diceBox = new JLabel();
-        diceBox.setPreferredSize(new Dimension(200, 100));
-        diceBox.setOpaque(true);
-        diceBox.setBackground(Color.blue);
+        diceBox = new ImagePanel(images.get("felt"), images.get("borderTL"), images.get("borderTR"), images.get("borderBL"), images.get("borderBR"), images.get("borderTop"), images.get("borderBottom"), images.get("borderLeft"), images.get("borderRight"));
+        diceBox.setPreferredSize(new Dimension(DICE_BOX_WIDTH, DICE_BOX_HEIGHT));
+
+        FlowLayout layout = new FlowLayout();
+        layout.setHgap(DICE_GAP);
+        diceBox.setLayout(layout);
+        diceBox.setBorder(new EmptyBorder(DICE_PADDING_TOP, 0, 0, 0));
+
+        die1 = new ImagePanel(images.get("die1"), false);
+        die1.setPreferredSize(new Dimension(DICE_SIZE, DICE_SIZE));
+        die2 = new ImagePanel(images.get("die1"), false);
+        die2.setPreferredSize(new Dimension(DICE_SIZE, DICE_SIZE));
+
+        diceBox.add(die1);
+        diceBox.add(die2);
 
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.NONE;
@@ -115,9 +171,7 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
     }
 
     private void buildCardBox(JPanel container) {
-        cardBox = new JLabel();
-        cardBox.setOpaque(true);
-        cardBox.setBackground(Color.red);
+        cardBox = new ImagePanel(images.get("felt"), images.get("borderTL"), images.get("borderTR"), images.get("borderBL"), images.get("borderBR"), images.get("borderTop"), images.get("borderBottom"), images.get("borderLeft"), images.get("borderRight"));
         cardBox.setPreferredSize(new Dimension(100, 150));
 
         GridBagConstraints c = new GridBagConstraints();
@@ -139,7 +193,7 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
 
         GridBagConstraints c = new GridBagConstraints();
         messageBox = new JLabel("Hello, here is a message!");
-        messageBox.setPreferredSize(new Dimension(200, 20));
+        messageBox.setMinimumSize(new Dimension(0, 20));
         messageBox.setOpaque(true);
         messageBox.setBackground(Color.cyan);
 
@@ -151,6 +205,7 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
 
         boardBox = new JLabel();
         boardBox.setOpaque(true);
+        setMinimumSize(new Dimension(600, 600));
         boardBox.setBackground(Color.green);
 
         c.fill = GridBagConstraints.BOTH;
@@ -198,6 +253,13 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
         }
     }
 
+    private void updateDice(DiceUpdate update) {
+        die1.setBacktroundImage(images.get("die" + update.FirstDie));
+        die1.repaint();
+        die2.setBacktroundImage(images.get("die" + update.SecondDie));
+        die2.repaint();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -206,9 +268,10 @@ public class GameWindow extends JFrame implements Observer, ActionListener {
     @Override
     public void update(Observable o, Object arg) {
         if(arg != null) {
-            if (arg instanceof PlayerRequest) {
+            if (arg instanceof PlayerRequest)
                 request((PlayerRequest)arg);
-            }
+            else if(arg instanceof DiceUpdate)
+                updateDice((DiceUpdate)arg);
         }
     }
 }
---
+++ source/cluedo/src/gui/ImagePanel.java
@@ -0,0 +1,99 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Jpanel that draws tiled background image
+ */
+public class ImagePanel extends JPanel {
+    private Image backtroundImage;
+    private Image borderTL, borderTR, borderBL, borderBR, borderTop, borderBottom, borderLeft, borderRight;
+    private boolean drawBorder, tile;
+    private int borderSize = 10;
+
+    public ImagePanel(Image backgroundImage, boolean tile) {
+        this.backtroundImage = backgroundImage;
+        drawBorder = false;
+        this.tile = tile;
+    }
+
+    public ImagePanel(Image backgroundImage, Image borderTL, Image borderTR, Image borderBL, Image borderBR, Image borderTop, Image borderBottom, Image borderLeft, Image borderRight) {
+        this.backtroundImage = backgroundImage;
+        this.borderTL = borderTL;
+        this.borderTR = borderTR;
+        this.borderBL = borderBL;
+        this.borderBR = borderBR;
+        this.borderTop = borderTop;
+        this.borderBottom = borderBottom;
+        this.borderLeft = borderLeft;
+        this.borderRight = borderRight;
+        drawBorder = true;
+        tile = true;
+    }
+
+    public void setBacktroundImage(Image image) {
+        this.backtroundImage = image;
+    }
+
+    /**
+     * Paints background image
+     * @param g
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        if(tile) {
+            drawBackgroundTiled(g);
+            drawBorder(g);
+        } else {
+            drawBackgroundStretched(g);
+        }
+    }
+
+    private void drawBackgroundStretched(Graphics g) {
+        if (backtroundImage == null ) return;
+
+        // Draw tiled image
+        Dimension dimensions = getSize();
+        g.drawImage( backtroundImage, 0, 0, dimensions.width, dimensions.height, null, null);
+    }
+
+    private void drawBackgroundTiled(Graphics g) {
+        if (backtroundImage == null ) return;
+
+        // Draw tiled image
+        Dimension dimensions = getSize();
+        for (int x = 0; x < dimensions.width; x += backtroundImage.getWidth(null)) {
+            for (int y = 0; y < dimensions.height; y += backtroundImage.getHeight(null)) {
+                g.drawImage( backtroundImage, x, y, null, null);
+            }
+        }
+    }
+
+    private void drawBorder(Graphics g) {
+        if(!drawBorder) return;
+
+        Dimension dimensions = getSize();
+
+        for (int x = 0; x < dimensions.width; x += borderTop.getWidth(null)) {
+            g.drawImage(borderTop, x, 0, borderTop.getWidth(null), borderSize,null, null);
+        }
+
+        for (int x = 0; x < dimensions.width; x += borderBottom.getWidth(null)) {
+            g.drawImage(borderBottom, x, dimensions.height - borderSize, borderBottom.getWidth(null), borderSize, null, null);
+        }
+
+        for (int y = 0; y < dimensions.height; y += borderLeft.getHeight(null)) {
+            g.drawImage(borderLeft, 0, y, borderSize, borderLeft.getHeight(null), null, null);
+        }
+
+        for (int y = 0; y < dimensions.height; y += borderRight.getHeight(null)) {
+            g.drawImage(borderRight, dimensions.width - borderSize, y, borderSize, borderRight.getHeight(null), null, null);
+        }
+
+        g.drawImage(borderTL, 0, 0, borderSize, borderSize, null, null);
+        g.drawImage(borderTR, dimensions.width - borderSize, 0, borderSize, borderSize,null, null);
+        g.drawImage(borderBL, 0, dimensions.height - borderSize, borderSize, borderSize,null, null);
+        g.drawImage(borderBR, dimensions.width - borderSize, dimensions.height - borderSize, borderSize, borderSize,null, null);
+    }
+}
