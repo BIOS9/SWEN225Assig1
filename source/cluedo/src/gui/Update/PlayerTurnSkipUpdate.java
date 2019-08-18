b69d07e467717f2ffd1ba138c893b2f1ed288d05
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-08-18 20:36:01 +1200

    adds board update constructor

--- source/cluedo/src/gui/Update/PlayerTurnSkipUpdate.java
+++ source/cluedo/src/gui/Update/PlayerTurnSkipUpdate.java
@@ -5,10 +5,9 @@ import game.Player;
 /**
  * Represents a player turn skip update, basic message for when a player cannot play thier turn for any reason
  * and is therefore considered skipped.
- * e.g. made an incorrect refutation and therefore may no longer move
+ * e.g. made an incorrect assumption and therefore may no longer move
  * @author abbey
- *
- *!!!!! have i got the right gist here?? (am just going through wrapping my head around all your stuff)
+ * 
  */
 public class PlayerTurnSkipUpdate extends GameUpdate {
     public final Player player;
--- source/cluedo/src/gui/Update/boardUpdate.java
+++ source/cluedo/src/gui/Update/boardUpdate.java
@@ -1,5 +1,19 @@
 package gui.Update;
 
-public class boardUpdate {
+import game.board.Board;
 
+/**
+ * Represents an update of the board during the game.
+ * @author abbey
+ */
+public class boardUpdate extends GameUpdate{
+	public final Board board;
+	
+	/**
+	 * Constructs a new board update
+	 * @param updated board
+	 */
+	public boardUpdate (Board board) {
+		this.board = board;
+	}
 }
