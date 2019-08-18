907fa84d4614d2d00ecd3cf9df55e24fd8c584a0
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-08-18 14:03:28 +1200

    adds compulsory public javadoc comments to request classes.

--- source/cluedo/src/gui/request/PlayerCountRequest.java
+++ source/cluedo/src/gui/request/PlayerCountRequest.java
@@ -2,9 +2,17 @@ package gui.request;
 
 import game.CluedoGame;
 
+/**
+ * Represents a new player count request for the start of the game. response must be between 3-6.
+ * @author abbey
+ *
+ */
 public class PlayerCountRequest extends PlayerRequest<Integer> {
     public final String conditions = "(3 - 6)"; // Condition string to present to player when asking for input
 
+    /**
+     * Checks validity of input, must be between 3(min players) and 6(max players).
+     */
     @Override
     public boolean isInputValid(Integer input) {
         return input >= CluedoGame.MIN_PLAYERS && input <= CluedoGame.MAX_PLAYERS;
