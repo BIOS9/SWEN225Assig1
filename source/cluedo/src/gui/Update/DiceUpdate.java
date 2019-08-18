fcaa78a1265e9639343dc3587e6485e110fcda3d
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-08-18 13:58:12 +1200

    adding compulsory java doc comments to public methods for game updates

--- source/cluedo/src/gui/Update/DiceUpdate.java
+++ source/cluedo/src/gui/Update/DiceUpdate.java
@@ -1,9 +1,19 @@
 package gui.Update;
 
+/**
+ * Class representing a dice update in the game.
+ * @author abbey
+ *
+ */
 public class DiceUpdate extends GameUpdate {
     public final int FirstDie;
     public final int SecondDie;
-
+    
+    /**
+     * Constructor updating the two dice in the game.
+     * @param firstDie
+     * @param secondDie
+     */
     public DiceUpdate(int firstDie, int secondDie) {
         this.FirstDie = firstDie;
         this.SecondDie = secondDie;
--- source/cluedo/src/gui/Update/PlayerTurnUpdate.java
+++ source/cluedo/src/gui/Update/PlayerTurnUpdate.java
@@ -2,10 +2,19 @@ package gui.Update;
 
 import game.Player;
 
+/**
+ * Represents a player turn update in the game.
+ * @author abbey
+ */
 public class PlayerTurnUpdate extends GameUpdate {
     public final Player player;
     public final int round;
 
+    /**
+     * Constructor for a new player turn update.
+     * @param player
+     * @param round
+     */
     public PlayerTurnUpdate(Player player, int round) {
         this.player = player;
         this.round = round;
