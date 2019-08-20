2e7b15eeacddf72238fb9b7402703d341aef5a65
berry-jam <52988535+berry-jam@users.noreply.github.com>
2019-08-20 20:28:42 +1200

    updates comments in update

--- source/cluedo/src/gui/Update/AllowedActionsUpdate.java
+++ source/cluedo/src/gui/Update/AllowedActionsUpdate.java
@@ -1,10 +1,21 @@
 package gui.Update;
 
+/**
+ * Represents the update package for allowed actions to be passed to the MVC.
+ * @author abbey
+ *
+ */
 public class AllowedActionsUpdate extends GameUpdate {
     public final boolean allowFinishTurn;
     public final boolean allowSuggestion;
     public final boolean allowAccusation;
-
+    
+    /**
+     * Constructs a new allowed actions update .
+     * @param allowFinishTurn
+     * @param allowSuggestion
+     * @param allowAccusation
+     */
     public AllowedActionsUpdate(boolean allowFinishTurn, boolean allowSuggestion, boolean allowAccusation) {
         this.allowFinishTurn = allowFinishTurn;
         this.allowSuggestion = allowSuggestion;
